6456c55439e802d2472affbf85bbc176fd4a49a3
Mya <myasenior13@outlook.com>
2020-10-08 18:07:54 +0000

    requirements2

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ from flask_pymongo import PyMongo
 from bson.objectid import ObjectId 
 
 app = Flask(__name__)
-app.config["MONGO_DBNAME"] = 'task_manger'
-app.config["MONGO_URI"] = 'mongodb+srv://rooot:'\
-                          'j6UF1blau6a1IeRz@myfirstcluster.'\
-                          'jwwdc.mongodb.net/task_manager?'\
-                          'retryWrites=true&w=majority'
+
+app.config["MONGO_DBNAME"] = 'task_manager'
+app.config["MONGO_URI"] = 'mongodb+srv://rooot:ovJpdy6ZxuMmEtWB@myfirstcluster.jwwdc.mongodb.net/task_manager?retryWrites=true&w=majority'
+app.secret_key = '@v3rts3cr3tk3y'
+
 
 mongo = PyMongo(app)
 
@@ -16,10 +16,22 @@ mongo = PyMongo(app)
 @app.route('/')
 @app.route('/get_tasks')
 def get_tasks():
-    return render_template("tasks.html", tasks=mongo.db.tasks.find())
+    tasks = mongo.db.tasks.find()
+    return render_template("tasks.html", tasks=tasks)
+
+
+@app.route('/add_task')
+def add_task():
+    return render_template('addtask.html')
 
 
 if __name__ == '__main__':
-    app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT')),
+    app.run(host='0.0.0.0',
+            port=5000,
             debug=True)
+
+
+# if __name__ == '__main__':
+#     app.run(host=os.environ.get('IP'),
+#             port=int(os.environ.get('PORT')),
+#             debug=True)
